e473df89ad011f65562bf04fcc73096500da13ed
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-10-06 12:29:33 +0530

    added utils module

---
+++ src/utils.py
@@ -0,0 +1,39 @@
+"""Small misc helpers shared across modules."""
+import os
+import random
+import numpy as np
+import torch
+
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+
+def device_from_cfg(cfg):
+    want = cfg.get("device", "cuda")
+    if want == "cuda" and not torch.cuda.is_available():
+        return torch.device("cpu")
+    return torch.device(want)
+
+
+def ensure_dir(path):
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+def to_device(obj, device):
+    if isinstance(obj, torch.Tensor):
+        return obj.to(device)
+    if isinstance(obj, dict):
+        return {k: to_device(v, device) for k, v in obj.items()}
+    if isinstance(obj, (list, tuple)):
+        return type(obj)(to_device(x, device) for x in obj)
+    return obj
